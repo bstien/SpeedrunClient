a0dc4ae396f40d4ad09dc8c8daa9e3ac770debe7
Bastian Stien <bastian.stien@gmail.com>
2020-05-21 13:07:42 +0200

    Add GameItemView

---
+++ SpeedrunCom Client/Views/Components/GameItemView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct GameItemView: View {
+    @State var game: Game
+    @State var geometryProxy: GeometryProxy
+
+    private var imageSize: CGFloat {
+        min(geometryProxy.size.width, geometryProxy.size.height) / 4
+    }
+
+    var body: some View {
+        HStack(alignment: .top) {
+            Group {
+                RemoteImageView(
+                    url: self.game.assets.coverMedium?.url,
+                    errorView: { Rectangle().fill(Color.red) },
+                    imageView: { $0.resizable() },
+                    loadingView: { Rectangle().fill(Color.gray) }
+                ).scaledToFill()
+            }
+            .frame(width: self.imageSize, height: self.imageSize)
+            .cornerRadius(8)
+
+            HStack(alignment: .top) {
+                Text(self.game.names.namePrioritized)
+                Spacer()
+                Text(DateFormatter.yyyyMMdd.string(from: self.game.releaseDate!)).font(.caption).italic()
+            }
+        }
+    }
+}
