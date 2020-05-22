8fcc002dca8c8901b4c206751b6ce92a5009f4ab
Bastian Stien <bastian.stien@gmail.com>
2020-05-22 13:56:13 +0200

    Pull filters into separate view

--- SpeedrunCom Client/Views/SpeedrunFilterView.swift
+++ SpeedrunCom Client/Views/SpeedrunFilterView.swift
@@ -5,10 +5,36 @@ struct SpeedrunFilterView: View {
 
     var body: some View {
         HStack {
-            Text(filterModel.runStatus.rawValue)
-            Button(action: {
-                self.filterModel.runStatus = self.filterModel.runStatus == .verified ? .new : .verified
-            }, label: { Text("Switch") })
+            FilterItemView(title: "Status", value: filterModel.runStatus.rawValue, action: { self.configureRunStatus() })
+            Spacer()
+            FilterItemView(title: "Order by", value: filterModel.orderBy.rawValue, action: { self.configureOrderBy() })
+            Spacer()
+            FilterItemView(title: "Sorting", value: filterModel.sorting.rawValue, action: { self.configureSorting() })
         }
+    }
+
+    private func configureRunStatus() {
+        self.filterModel.runStatus = .verified
+    }
+
+    private func configureOrderBy() {
+        self.filterModel.orderBy = .platform
+    }
+
+    private func configureSorting() {
+        self.filterModel.sorting = .asc
+    }
+}
+
+private struct FilterItemView: View {
+    private(set) var title: String
+    private(set) var value: String
+    private(set) var action: () -> Void
+
+    var body: some View {
+        VStack {
+            Text(title).fontWeight(.bold)
+            Text(value)
+        }.onTapGesture(perform: action)
     }
 }
